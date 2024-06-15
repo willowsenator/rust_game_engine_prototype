16365a50031a7027a935a6363bc7bb290e09b2be
willownsenator <willownsenator@gmail.com>
2024-06-16 00:47:50 +0200

    Add player Collision and increment score

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,35 @@ fn main() {
     let mut game = Game::new();
 
     let player = game.add_sprite("player", SpritePreset::RacingCarGreen);
-    player.translation = Vec2::new(400.0, 300.0);
+    player.translation = Vec2::new(0.0, 0.0);
+    player.rotation = NORTH_EAST;
+    player.scale = 1.0;
+    player.layer = 1.0;
+    player.collision = true;
+
+    let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
+    car1.translation = Vec2::new(300.0, 0.0);
+    car1.collision = true;
 
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-    game_state.current_score += 1;
+    for event in engine.collision_events.drain(..) {
+        if event.state.is_begin() && event.pair.one_starts_with("player") {
+            // remove the sprite the player collided with
+            for label in [event.pair.0, event.pair.1] {
+                if label != "player" {
+                    engine.sprites.remove(&label);
+                }
+            }
+
+            game_state.current_score += 1;
+            println!("Current Score: {}", game_state.current_score);
+        }
+    }
+
+    let player = engine.sprites.get_mut("player").unwrap();
+    player.translation.x += 100.0 * engine.delta_f32;
 }
